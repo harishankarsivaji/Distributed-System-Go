c50b8c449978d211379488c0b64e6caf15deaf58
harishankarsivaji <harishs92@gmail.com>
2020-06-18 11:11:03 +0100

    Init Go mod Decalred a message queue with amqp

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/harishankarsivaji/Distributed-System-Go
+
+go 1.14
+
+require github.com/streadway/amqp v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,39 @@
 package Distributed_System_Go
+
+import (
+	"fmt"
+	"github.com/streadway/amqp"
+	"log"
+)
+
+func main() {
+
+}
+
+func getQueue() (*amqp.Connection, *amqp.Channel, *amqp.Queue) {
+	//establishing connection with RabbitMQ
+	conn, err := amqp.Dial("amqp://guest@locahost:15672")
+	failOnError(err, "Failed to connect to RabbitMQ")
+
+	// opening a Channel
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a Channel")
+
+	//
+	q, err := ch.QueueDeclare("Hello world",
+		false,
+		false,
+		false,
+		false,
+		nil)
+	failOnError(err, "Failed to declare a queue")
+
+	return conn, ch, &q
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", err, msg))
+	}
+}
